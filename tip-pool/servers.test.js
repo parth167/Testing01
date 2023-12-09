4bf5986b940b1035bd1c796ad28131a00a9ebaae
patel <parthpatel167@gmail.com>
2023-12-09 13:28:52 -0600

    add some test to server test

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,4 +1,4 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe('Servers test (with setup and tear-down)', function () {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
@@ -10,8 +10,18 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(Object.keys(allServers).length).toEqual(1);
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
-
-  afterEach(function() {
+  // alteast one server needed
+  it('should not  add a new server with empty value to allServers on submitServerInfo()', function () {
+    serverNameInput.value = '';
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+  //
+  //Q -> if i use after each with line 17 .toequal(1) faile my test but if i do not use aftereach line 17 .toequal(0)throw an error
+  afterEach(function () {
     // teardown logic
+    serverId = 0;
+    serverTbody.innerHTML = '';
+    allServers = {};
   });
 });
